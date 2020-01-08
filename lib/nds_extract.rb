7eab0dc90364e75e32fd69901f25a83a4ab7602d
fadimata tillman <fadi.tillman@gmail.com>
2020-01-08 16:31:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -33,15 +33,15 @@ p directors_database
   #
   grand_total = 0
 row_index = 0
-while row_index < directors_to.length do
+while row_index < directors_totals.length do
   column_index = 0
-  while column_index < vm[row_index].length do
-    inner_len = vm[row_index][column_index].length
+  while column_index < directors_totals[row_index].length do
+    inner_len = directors_totals[row_index][column_index].length
     inner_index = 0
     while inner_index < inner_len do
     #total directors 
    
-    grand_total += vm[row_index][column_index][inner_index][:price]
+    grand_total += directors_totals[row_index][column_index][inner_index][:price]
       inner_index += 1
     end
     column_index += 1
@@ -49,7 +49,7 @@ while row_index < directors_to.length do
   row_index += 1
 end
  
-p grand_tota  
+p grand_total  
   # Be sure to return the result at the end!
   nil
 end
