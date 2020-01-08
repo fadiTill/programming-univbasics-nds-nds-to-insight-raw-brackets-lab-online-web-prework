7c7d844753a7097cd575bd8ddfc23b1318d54818
fadimata tillman <fadi.tillman@gmail.com>
2020-01-08 16:14:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,6 +5,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 def directors_totals(nds)
   [ ]
+  p 'directors_database'
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
